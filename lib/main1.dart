90706fbcd63bc701690c6c5b0e3c21c49e293c39
misael <misaeldessalegn@gmail.com>
2024-09-16 19:44:53 +0300

    final

--- lib/main1.dart
+++ lib/main1.dart
@@ -1,21 +1,13 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:video_player/video_player.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
 import 'package:path/path.dart' as path;
-import 'package:media_kit/media_kit.dart';
-import 'package:media_kit_video/media_kit_video.dart';
 
 void main() {
-    WidgetsFlutterBinding.ensureInitialized();
-  // Make sure to add the required packages to pubspec.yaml:
-  // * https://github.com/media-kit/media-kit#installation
-  // * https://pub.dev/packages/media_kit#installation
-  MediaKit.ensureInitialized(
-
-  );
   runApp(const MyApp());
 }
 
@@ -25,12 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
-  // Make sure to add the required packages to pubspec.yaml:
-  // * https://github.com/media-kit/media-kit#installation
-  // * https://pub.dev/packages/media_kit#installation
-
       debugShowCheckedModeBanner: false,
+      
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -49,8 +37,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String? _directoryPath;
   List<File> _videoFiles = [];
-  Player? _player; // Updated player controller for media_kit
-  VideoController? _videoController;
+  VideoPlayerController? _controller;
   String? _thumbnailsDir;
   bool _isPlaying = false;
 
@@ -123,18 +110,15 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> _playVideo(File file) async {
-    if (_player != null) {
-      await _player!.dispose();
+    if (_controller != null) {
+      await _controller!.dispose();
     }
-    _player = Player(); // Create media_kit Player instance
-    _videoController = await VideoController(_player!);
-    await _player!.open(Media(file.path)); // Load video from file
-
+    _controller = VideoPlayerController.file(file);
+    await _controller!.initialize();
     setState(() {
       _isPlaying = true;
     });
-
-    await _player!.play();
+    await _controller!.play();
   }
 
   @override
@@ -143,29 +127,32 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text('Pathology Videos'),
         centerTitle: true,
-        leading: _isPlaying
-            ? IconButton(
-                onPressed: () {
-                  setState(() {
-                    _isPlaying = false;
-                    _player?.pause();
-                  });
-                },
-                icon: const Icon(Icons.arrow_back),
-              )
-            : Text(''),
+ 
+      leading:  _isPlaying ? IconButton(
+          onPressed: () {
+            setState(() {
+              _isPlaying = false;
+              _controller!.pause();
+            });
+          },
+          icon: const Icon(Icons.arrow_back),
+        ): Text(''),
+         
         actions: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              _isPlaying
-                  ? const Text('')
-                  : TextButton(
-                      child: const Text('.'),
-                      onPressed: _selectDirectory,
-                    ),
+              
+         _isPlaying ?const Text('') : 
+          TextButton(
+
+             child:  const Text('.'),
+            onPressed: _selectDirectory,
+          ),
+
             ],
           ),
+        
         ],
       ),
       body: _isPlaying ? _buildVideoPlayer() : _buildVideoList(),
@@ -173,15 +160,15 @@ class _HomePageState extends State<HomePage> {
           ? FloatingActionButton(
               onPressed: () {
                 setState(() {
-                  if (_player!.state.playing) {
-                    _player!.pause();
+                  if (_controller!.value.isPlaying) {
+                    _controller!.pause();
                   } else {
-                    _player!.play();
+                    _controller!.play();
                   }
                 });
               },
               child: Icon(
-                _player!.state.playing ? Icons.pause : Icons.play_arrow,
+                _controller!.value.isPlaying ? Icons.pause : Icons.play_arrow,
               ),
             )
           : null,
@@ -190,20 +177,20 @@ class _HomePageState extends State<HomePage> {
 
   Widget _buildVideoPlayer() {
     return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.9,
-      child: Column(
-        children: [
-          Expanded(
-            child: AspectRatio(
-              aspectRatio:MediaQuery.of(context).size.width * 9.0 / 16.0,
-              child: Video(
-                controller: _videoController!,
-              ),
-            ),
+    height: MediaQuery.of(context).size.height * 0.9,
+    child: Column(
+      children: [
+        Expanded(
+          child: AspectRatio(
+            aspectRatio: _controller!.value.aspectRatio,
+            child: VideoPlayer(_controller!),
           ),
-        ],
-      ),
-    );
+        ),
+        VideoProgressIndicator(_controller!, allowScrubbing: true),
+    
+      ],
+    ),
+  );
   }
 
   Widget _buildVideoList() {
@@ -211,7 +198,7 @@ class _HomePageState extends State<HomePage> {
       return const Center(child: Text('Please select a directory'));
     }
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      padding: const EdgeInsets.symmetric(horizontal:  8.0),
       child: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
@@ -222,7 +209,7 @@ class _HomePageState extends State<HomePage> {
         itemCount: _videoFiles.length,
         itemBuilder: (context, index) {
           final file = _videoFiles[index];
-          final videoTitle = extractVideoTitle(file.path);
+            final videoTitle = extractVideoTitle(file.path); 
           return GestureDetector(
             onTap: () => _playVideo(file),
             child: FutureBuilder<String>(
@@ -234,7 +221,7 @@ class _HomePageState extends State<HomePage> {
                     fit: StackFit.expand,
                     children: [
                       Image.asset(
-                        'assets/thumb48.png',
+                      'assets/thumb48.png',
                         fit: BoxFit.cover,
                       ),
                       const Center(
@@ -248,12 +235,11 @@ class _HomePageState extends State<HomePage> {
                     children: [
                       SizedBox(
                         height: 150,
-                        child: Image.asset(
+                        child:Image.asset(
                           'assets/thumb96.png',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Text(videoTitle.toString()),
+                            fit: BoxFit.cover,
+                          ),),
+                          Text(videoTitle.toString()),
                     ],
                   );
                 }
@@ -267,13 +253,12 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
-    _player?.dispose();
+    _controller?.dispose();
     super.dispose();
   }
-
-  String extractVideoTitle(String filePath) {
-    final fileName = filePath.split('\\').last;
-    final title = fileName.substring(0, fileName.lastIndexOf('.'));
-    return title;
-  }
+ String extractVideoTitle(String filePath) {
+  final fileName = filePath.split('\\').last; // Get the file name
+  final title = fileName.substring(0, fileName.lastIndexOf('.')); // Remove the extension
+  return title;
+}
 }
